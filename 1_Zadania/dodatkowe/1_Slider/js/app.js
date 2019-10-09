7b8b4ccfbe8af940dd1633c256e010d8e394eb3b
Nitril <athlonxp@poczta.fm>
2019-10-09 23:14:31 +0200

    Zadanie 1 dodatkowe

---
+++ 1_Zadania/dodatkowe/1_Slider/js/app.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('Działa!');
+    var imgIndex = 0;
+    var next = document.getElementById("nextPicture");
+    var prev = document.getElementById("prevPicture");
+    
+    //const list = document.querySelector('ul');
+    //document.getElementsByClassName("slider")[0].classList.add('is-visible');
+    //photoArray[index]. = "visible";
+    //[index].
+    //photoArray = document.getElementsByClassName("slider").getElementsByTagName('li')[0];
+    photoArray = document.getElementsByClassName('slider')[0].getElementsByTagName('li'); 
+    photoArray[index].classList.add('visible'); 
+    console.log(photoArray[index]);
+    console.log(prev);
+
+    console.log(next);
+       
+
+    function nextClicked() {
+        photoArray[imgIndex].classList.remove('visible');
+        console.log("next clicked");
+        if  (imgIndex >= photoArray.length-1) index = 0;
+        else imgIndex++;
+        photoArray[imgIndex].classList.add('visible'); 
+    }
+
+    function prevClicked() {
+        photoArray[imgIndex].classList.remove('visible');
+        console.log("prev clicked");
+        if  (imgIndex <= 0) imgIndex = photoArray.length-1;
+        else imgIndex--;
+        photoArray[imgIndex].classList.add('visible'); 
+    }
+
+    next.addEventListener("click", nextClicked );
+    prev.addEventListener("click", prevClicked);
+
+    
+});
+
+
+    
---
+++ 1_Zadania/obowiązkowe/2_Tablice/js/app.js
@@ -0,0 +1,156 @@
+//Zadanie 0
+function distFromAvarage(inputArray) {
+    
+    var sum = 0;
+
+    for (var i = 0; i < inputArray.length; i++) {
+        sum += inputArray[i];
+
+    }    
+
+    var avarage = sum/inputArray.length;
+    var outputArray = [];
+
+    for (var i = 0; i < inputArray.length; i++) {
+        outputArray.push(inputArray[i] - avarage); 
+
+    }
+
+    return outputArray;
+}
+
+distFromAvarage([1,2,3,4,5,6,7]);
+distFromAvarage([1,1,1,1]);
+distFromAvarage([2,8,3,7]); 
+
+//Zadanie 1
+var array=["gruszka", "jablko", "mango", "ananas"];
+
+console.log(array[0]);
+console.log(array[array.length-1]);
+
+for(var i=0; i<array.length;i++){
+
+    console.log(array[i]);
+
+}
+
+
+//Zadanie 3
+function printTable(array) {
+
+    for(var i=0; i<array.length;i++){
+
+        console.log(array[i]);
+    
+    }
+
+}
+
+printTable([1,2,3,4]);
+
+
+//Zadanie 4
+function multiply(array) {
+
+    var multiplication = 1;
+
+    for (let i = 0; i < array.length; i++) {
+        multiplication *= array[i];
+        
+    }
+
+    return multiplication;
+
+}
+
+
+//Zadanie 5
+function getEvenAvarage(array) {
+    
+    var sum = 0;
+    var counter = 0;
+
+    for (let i = 0; i < array.length; i++) {
+
+        if (array[i] % 2 === 0) {
+            sum += array[i];
+            counter++;
+
+        }
+        
+    }
+
+    if (counter == 0) return null;
+    return sum/counter;
+
+}
+
+getEvenAvarage([1,2,3,4,5,6,7]);s
+getEvenAvarage([1,1,1,1]);
+getEvenAvarage([2,8,3,7,4]);
+
+//Zadanie 6
+function sortArray(array) {
+
+    /* 
+    function forCompare(a, b) {
+        return a - b;
+    }
+    */ 
+     
+    return array.sort((a,b) => a-b);
+        
+
+}
+
+sortArray([145,11,3,64,4,6,10]);
+
+//Zadanie 7
+function addArrays(array, array2) {
+
+    var length = 0;
+    var compare = array.length - array2.length;
+    
+    if (compare > 0 )  { 
+
+        length = array.length
+        
+    }  
+    else  {
+
+        length = array2.length
+
+    }
+        
+    var outputArray = [];
+
+    for (let i = 0; i < length; i++) {
+        
+        if (compare == 0 || i < length - Math.abs(compare))  {
+            outputArray[i] = array[i] + array2[i];
+            
+        }  
+        else  {
+    
+            if(compare > 0) {   //if array is longer  than array2
+
+                outputArray[i] = array[i];
+            }
+            else {
+
+                outputArray[i] = array2[i];
+            }
+    
+        }
+        
+        
+    }
+
+    return outputArray;
+
+}
+
+addArrays([4,0,1,3,4], [1,9,6,7,8,17]);
+addArrays([8,3,22], [1,3,2]);
+addArrays([2,3,1,5,3,5], [3,1,76,1]);
