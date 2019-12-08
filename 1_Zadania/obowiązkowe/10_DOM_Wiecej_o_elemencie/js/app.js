9e5e5adf66b691c5b480703b4667d557b96edfc5
Szarek Karol <karol.szarek@microsoft.wsei.edu.pl>
2019-12-08 19:13:38 +0100

    Lab 8.12.2019

--- 1_Zadania/obowiązkowe/10_DOM_Wiecej_o_elemencie/js/app.js
+++ 1_Zadania/obowiązkowe/10_DOM_Wiecej_o_elemencie/js/app.js
@@ -3,4 +3,34 @@ document.addEventListener("DOMContentLoaded", function () {
     Poniżej napisz kod rozwiązujący zadania
      */
 
+    //Zadanie 0
+    var ex5 = document.querySelectorAll('.ex5 li');
+    
+    for (let i = 0; i < ex5.length; i++) {
+        if (i === 0 || i % 2 === 0) ex5[i].style.backgroundColor = "green";
+        if (i % 5 === 0) ex5[i].classList.toggle("big");
+        if (i % 3 === 0) ex5[i].style.textDecoration = "underline";
+    }
+
+
+    //Zadanie 1
+
+    var chrome = document.querySelector('.chrome');
+    var edge = document.querySelector('.edge');
+    var firefox = document.querySelector('.firefox');
+
+    chrome.style.backgroundRepeat = 'no-repeat';
+    edge.style.backgroundImage = "url('./assets/img/edge.png')";
+    firefox.style.backgroundImage = "url('./assets/img/firefox.png')";
+
+    chrome.style.width = '100px';
+    chrome.style.height = '100px';
+
+    chrome.nextElementSibling.innerHTML =  'Chrome'; //zwraca element nie obiekt
+    chrome.nextElementSibling.setAttribute('href', 'http://chrome.com');
+    edge.nextElementSibling.innerHTML =  'Edge'; //zwraca element nie obiekt
+    edge.nextElementSibling.setAttribute('href', 'http://edge.com');
+    firefox.nextElementSibling.innerHTML =  'Firefox'; //zwraca element nie obiekt
+    firefox.nextElementSibling.setAttribute('href', 'http://firefox.com');
+    //backgroundRepeat
 });
--- 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
+++ 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
@@ -4,9 +4,117 @@ document.addEventListener("DOMContentLoaded", function () {
     var banner = document.querySelector(".ban");
     var blocks = document.querySelectorAll(".block");
     var links = document.querySelector(".links").children;
+    console.log(links);
 
+    function getDatasInfo(elements) {
+        var newArray = [];
+        
+        for (let i = 0; i < array.length; i++) {
+            newArray.push(elements[i].getAttribute('data-color'));
+            
+        }
+        console.log(newArray);
+        return newArray;
+
+
+    }
     /*
     Poniżej napisz kod rozwiązujący zadania
      */
+    getDatasInfo(links);
+
+    //Zadanie 1
+    console.log(homeElement);
+    console.log(childElements);
+    console.log(banner);
+    console.log(blocks);
+    console.log(links);
+
+    for (let i = 0; i < childElements.length; i++) {
+        console.log(childElements.tagName, childElements.classList);
+        
+    }
+
+    for (let i = 0; i < blocks.length; i++) {
+        console.log(blocks.tagName, blocks.classList);
+        
+    }
+
+
+    for (let i = 0; i < links.length; i++) {
+        console.log(links.tagName, links.classList);
+        
+    }
+
+
+    //Zadanie 2
+
+    for (el of blocks) {
+        console.log(el.innerHTML);
+        console.log(el.outerHTML);
+        el.innerHTML = "Nowa wartość diva o klasie blocks"
+        console.log(el.innerHTML);
+        console.log(el.outerHTML);
+    }
+
+
+    //Zadanie 3
+
+    var mainFooter = document.querySelector('#mainFooter');
+    function getID(element){
+        console.log(element.id);
+        return element.id;
+
+    }
+
+    getID(mainFooter);
+
+    //Zadanie 4
+
+    function getTags(elements) {
+        let arr1 = [];
+
+        for (let i = 0; i < elements.length; i++) {
+            arr1.push(elements[i].childElements.tagName);
+            
+        }
+        return childElements
+
+    }
+
+    getTags(childElements);
+
+    //Zadanie 5
+
+    function getClassInfo(element) {
+        let arr = [];
+
+        for (let i = 0; i < element.classList.length; i++) {
+            arr.push(element.classList[i]);
+            
+        }
+        console.log(arr);
+        return arr;
+
+    }
+
+    getClassInfo(banner);
+
+    //Zadanie 6
+
+    var liElements = document.querySelectorAll('new li');
+
+    function setDataDirection(elements){
+        for(item of elements) {
+            if(!item.getAttribute('data-direction')){
+                item.setAttribute('data-direction', 'top');
+
+            }
+
+        }
+
+
+    }
+    setDataDirection();
 
 });
