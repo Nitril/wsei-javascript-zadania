62e3dd9c776d933e210360e85f4ee61efde4050f
Karol <karol.szarek@gmail.com>
2019-12-25 16:15:24 +0100

    Dodatkowe 2 - gallery

--- 1_Zadania/dodatkowe/2_Gallery/js/app.js
+++ 1_Zadania/dodatkowe/2_Gallery/js/app.js
@@ -1,61 +1,61 @@
-
+/*
+ <div class="fullScreen">
+   <img src="./images/wrong.png">
+   <button class="close">Close</button>
+ </div>
+ */
 document.addEventListener('DOMContentLoaded', myCode, false);
-function* makeRangeIterator(start = 0, end = 100, step = 1) {
-    i = start;
-    while(true) {
+// function* makeRangeIterator(start = 0, end = 100, step = 1) {
+//     i = start;
+//     while(true) {
 
-        yield i%2;
-        i++
-    }
-}
+//         yield i%2;
+//         i++
+//     }
+// }
 
 
 
 function myCode() {
     console.log(`DOMContentLoaded\nDziała`);
-    var lista = document.getElementsByClassName("gallery")[0].getElementsByTagName('li');
-    
-    for(var i = 0; i < lista.length; i++){
-        // Console: print the clicked <p> element 
-        lista[i].addEventListener('click', handleMouseClick, false);
-         
-    }
-    
+    let gal = document.getElementsByClassName("gallery")[0].getElementsByTagName('li');
+    //let lista = [];
+    //for(var i = 0, a = []; i < gal.length; i++) lista.push(gal[i]);
+    let lista = Array.from(document.getElementsByClassName("gallery")[0].getElementsByTagName('li'));
+        
     var body = document.body;
     
     console.log(lista);
     console.log(body);
     
-    
+    for(let i = 0; i < lista.length; i++){
+        
+        lista[i].addEventListener('click', handleMouseClick, false);
+         
+    }
+
     function handleMouseClick(e) { 
     
-        
-        
-        //getElementsByTagName('img')[0].src
-        var img = this.querySelector('img');   
+        let img = this.querySelector('img');   
         console.log(img.src); 
-        //document.body.style.background = "#f3f3f3 url('${this.querySelector('img').src}') no-repeat right top";
-        //body.style.backgound.url = this.querySelector('img').src;
-        //body.style.backgroundImage = "url('"+img.src+"')";
-        //img.requestFullscreen();
-        //img.style.display = 'block';
-        
-        var imgc = document.createElement('img'); 
-        imgc.src = img.src; 
-
-        
-        //bdy.replaceChild(img, imgc);
-        //var x = func();
-        //console.log(gen().next())
-        
-        if (document.body.hasChildNodes()) {
-            document.body.removeChild(document.body.lastChild);
-            
-         } 
-        else document.body.appendChild(imgc);
-            // while (body.hasChildNodes()) {  
                 
-            //   }
+        let div = document.createElement('div');
+        div.classList.add('fullScreen');
+        let imgc = document.createElement('img');
+        imgc.src = img.src;
+        let button = document.createElement('button');
+        button.classList.add('close');
+        let close = document.createTextNode('Close');
+        button.appendChild(close);
+        div.appendChild(imgc);
+        div.appendChild(button);
+        body.appendChild(div)
+        
+        button.addEventListener('click', function() {
+
+            body.removeChild(div);
+        }, false);
+        
          
          
     }
