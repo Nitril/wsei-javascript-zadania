8f15546797511eb88ad74d39276ff82598cad263
Karol <karol.szarek@gmail.com>
2019-12-27 12:55:43 +0100

    Dodatkowe 4

---
+++ 1_Zadania/dodatkowe/4_Menu/js/app.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', myCode, false);
+
+
+function myCode() {
+    console.log("dziala");
+    //var menu = document.getElementsByClassName("menu")[0].getElements
+    var li = Array.from(document.querySelectorAll(".nav > ul > li"));
+    console.log(li);
+
+    for (let i = 0; i < li.length; i++) {
+        li[i].addEventListener("mouseover", function() {
+            
+            var subList = this.querySelector("ul");
+            if (subList != null) {
+                //console.log(subList);
+                //console.log("mousover");
+                subList.style.display = "inline-block";
+            }
+        }, false);
+        
+        
+        li[i].addEventListener("mouseout", function() {
+            console.log("mouseout");
+            var subList = this.querySelector("ul");
+            if (subList != null) {
+                //console.log(subList);
+                //console.log("mousover");
+                subList.style.display = "none";
+            }
+        }, false);
+    }
+}
